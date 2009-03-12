199466463b2083a925817828fad9b423c266f7f8
Keith McDonnell <keith@dancingtext.com>
2009-03-12 17:32:51 +0100

    Subtotal for feature estimates

--- test/functional/features_view_test.rb
+++ test/functional/features_view_test.rb
@@ -1,49 +1,55 @@
 require 'test_helper'
 
 #  REST actions: new, create, delete, index
-class IndexViewTest < ActionController::TestCase
-  tests FeaturesController
-  
-  def setup
-    get :index
-  end
-  
-  def each_feature
-    @scrumspace.features.each { |f| yield f }
-  end
-  
-  test "should have a form to create a new feature" do
-    assert_select "form[action=/features]", 1
-  end
-  
-  test "should have a form to update each existing feature" do
-    each_feature do |f| 
-      assert_select "form[action=/features/#{f.id}]", 1 
+module View
+  class IndexTest < ActionController::TestCase
+    tests FeaturesController
+    
+    def setup
+      get :index
     end
-  end
-  
-  test "should have a submit button" do
-    each_feature do |f| 
-      assert_select "form[action=/features/#{f.id}] button", 1
+    
+    def each_feature
+      @scrumspace.features.each { |f| yield f }
     end
-  end
-  
-  test "should have textarea description" do
-    each_feature do |f|
-      assert_select "form[action=/features/#{f.id}] textarea[name=?]", /feature\[description\]/
+    
+    test "should have a form to create a new feature" do
+      assert_select "form[action=/features]", 1
     end
-  end
-  
-  test "should have number select for estimate" do
-    each_feature do |f|
-      assert_select "form[action=/features/#{f.id}] select[name=?]", /feature\[estimate\]/
+    
+    test "should have a form to update each existing feature" do
+      each_feature do |f| 
+        assert_select "form[action=/features/#{f.id}]", 1 
+      end
     end
-  end
-  
-  test "should have date select for demo" do
-    each_feature do |f|
-      # FIXME: find out how to match rails date form helper output
-      # assert_select "form[action=/features/#{f.id}] select[name=?]", /feature\[estimate\]/
+    
+    test "should have a submit button" do
+      each_feature do |f| 
+        assert_select "form[action=/features/#{f.id}] button", 1
+      end
+    end
+    
+    test "should have textarea description" do
+      each_feature do |f|
+        assert_select "form[action=/features/#{f.id}] textarea[name=?]", /feature\[description\]/
+      end
+    end
+    
+    test "should have number select for estimate" do
+      each_feature do |f|
+        assert_select "form[action=/features/#{f.id}] select[name=?]", /feature\[estimate\]/
+      end
+    end
+    
+    test "should have date select for demo" do
+      each_feature do |f|
+        # FIXME: find out how to match rails date form helper output
+        # assert_select "form[action=/features/#{f.id}] select[name=?]", /feature\[estimate\]/
+      end
+    end
+    
+    test "should have an subtotal of all estimations" do
+      assert_select "#subtotal", @scrumspace.features.sum(:estimate)
     end
   end
 end
