d55858e1abbca118c3dbabbffc5d7eade6fe9bd8
Keith McDonnell <keith@dancingtext.com>
2009-03-16 12:54:05 +0100

    For dev convience. App root will eventually be landing page

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,8 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :sprints
   map.resources :tasks, :collection => { :filter => :get }
   
+  map.root :controller => 'features'
+  
   # Sample resource route with options:
   #   map.resources :products, :member => { :short => :get, :toggle => :post }, :collection => { :sold => :get }
 
