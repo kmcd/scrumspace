0db1f7d3feadb84674278f59ecc509a223f57c17
Keith McDonnell <keith@dancingtext.com>
2009-04-02 17:39:26 +0200

    Tasks blank slate OK

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,7 +1,7 @@
 module TasksHelper
   # TODO: dry up these helpers
   def team_select_options(product)
-    prompt =  options_for_select("All people") 
+    prompt =  options_for_select("Everyone") 
     prompt += options_from_collection_for_select(product.team, :dup , :dup, params[:owner])
   end
   
@@ -16,5 +16,13 @@ module TasksHelper
   
   def update_subtotal_on(page)
     page.replace_html 'subtotal', @product.tasks.sum(:remaining).to_s
+  end
+  
+  def blank_slate?
+    @product.tasks.empty? 
+  end
+  
+  def new_task_visability
+    "display: none;" unless blank_slate?
   end
 end
